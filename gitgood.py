a78869eb9511307806fc81b02ee9916480bfc5e6
B3nac <kyle.benac@gmail.com>
2024-08-30 17:05:36 -0700

    Dynamically set network values

--- gitgood.py
+++ gitgood.py
@@ -25,7 +25,6 @@ commits_db = "/commits.db"
 numbers = string.digits
 random = "".join(secrets.choice(numbers) for i in range(8))
 
-
 @click.command()
 @click.option(
     "--project-name",
@@ -54,20 +53,8 @@ def main(project_name, git_repo_path, payment_signing_key_path, network_type):
     payment_signing_key = PaymentSigningKey.load(payment_signing_key_path)
     payment_verification_key = PaymentVerificationKey.from_signing_key(
         payment_signing_key)
-
-    if network_type == "mainnet":
-        api = BlockFrostApi(project_id=os.environ["PROJECT_ID"], base_url=ApiUrls.mainnet.value)
-        if api.base_url == "https://cardano-mainnet.blockfrost.io/api":
-            # Use mainnet
-            network = Network.MAINNET
-    if network_type == "preprod":
-        api = BlockFrostApi(project_id=os.environ["PROJECT_ID"], base_url=ApiUrls.preprod.value)
-        if api.base_url == "https://cardano-preprod.blockfrost.io/api":
-            # Use testnet 
-            network = Network.TESTNET
-
+    api, network = get_network_attributes(network_type)
     from_address = str(Address(payment_verification_key.hash(), network=network))
-
     try:
         check_for_conflicts = check_output(
             ["git", "-C", f"{git_repo_path}", "diff", "main...origin/main"],
@@ -125,7 +112,8 @@ def main(project_name, git_repo_path, payment_signing_key_path, network_type):
                     create_metadata,
                     random,
                     local_commit_hash,
-                    last_id
+                    last_id,
+                    network_type
                 )
             else:
                 onchain_id = connection.execute(
@@ -162,7 +150,8 @@ def main(project_name, git_repo_path, payment_signing_key_path, network_type):
                         create_metadata,
                         onchain_id[0],
                         local_commit_hash,
-                        last_id
+                        last_id,
+                        network_type
                     )
     except CalledProcessError as e:
         print(e.output)
@@ -176,6 +165,17 @@ def get_db_connection():
     created_connection.row_factory = sqlite3.Row
     return created_connection
 
+
+def get_network_attributes(network_type):
+    if network_type == "mainnet":
+        api = BlockFrostApi(project_id=os.environ["PROJECT_ID"], base_url=ApiUrls.mainnet.value)
+        if api.base_url == "https://cardano-mainnet.blockfrost.io/api":
+            network = Network.MAINNET
+    if network_type == "preprod":
+        api = BlockFrostApi(project_id=os.environ["PROJECT_ID"], base_url=ApiUrls.preprod.value)
+        if api.base_url == "https://cardano-preprod.blockfrost.io/api":
+            network = Network.TESTNET
+            return api, network
 
 def get_metadata(
     onchain_id, project_name, local_commit_hash, commit_message, timestamp
@@ -217,7 +217,7 @@ def get_metadata(
 
 
 def send_transaction(
-    from_address, payment_signing_key, created_metadata, onchain_id, local_commit_hash, last_id
+    from_address, payment_signing_key, created_metadata, onchain_id, local_commit_hash, last_id, network_type
 ):
     context = BlockFrostChainContext(
         os.environ["PROJECT_ID"], base_url=ApiUrls.preprod.value
@@ -253,12 +253,11 @@ def send_transaction(
     )
     get_connection.close()
     time.sleep(80)
-    verify_commits_onchain(onchain_id, local_commit_hash)
+    verify_commits_onchain(onchain_id, local_commit_hash, network_type)
 
 
-def verify_commits_onchain(onchain_id, local_commit_hash):
-    # Change this to somehow dynamically set api type
-    api = BlockFrostApi(project_id=os.environ["PROJECT_ID"], base_url=ApiUrls.preprod.value)
+def verify_commits_onchain(onchain_id, local_commit_hash, network_type):
+    api, network = get_network_attributes(network_type)
     onchain_commits = api.metadata_label_json(onchain_id, return_type="json")
     for commit in onchain_commits:
         onchain_commit_hash = commit["json_metadata"]["msg"][1]
